24ce1178a33c7b6785535f4aa76c7874f12605f1
inhanbyeol <inhanbyeol94@gmail.com>
2023-08-16 18:05:16 +0900

    [추가] ejs 엔진 설정, validationPipe 설정, cookieParser 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(cookieParser());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  app.useStaticAssets(join(__dirname, '..', 'src', '_views'));
+  app.setBaseViewsDir(join(__dirname, '..', 'src', '_views'));
+  app.setViewEngine('ejs');
+
   await app.listen(3000);
 }
 bootstrap();
