932e6f541c4a0465a7b7bd9c412b92dd2052db1f
shimjaedoo <woen1637@naver.com>
2023-08-17 15:16:54 +0900

    test

--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -4,18 +4,19 @@ import { Repository } from 'typeorm';
 import { Member } from '../_common/entities/member.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { IResult } from '../_common/interfaces/return.interface';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class MemberService {
   constructor(@InjectRepository(Member) private membersRepository: Repository<Member>) {}
   //회원가입
-  async createMember(createMemberDto: CreateMemberDto): Promise<IResult> {
-    const { email } = createMemberDto;
+  async createMember(createMember: CreateMemberDto): Promise<IResult> {
+    const { email } = createMember;
     const existingUser = await this.membersRepository.findOne({ where: { email } });
 
     if (existingUser) throw new HttpException('이미 존재하는 이메일 입니다.', 403);
-
-    const newMember = this.membersRepository.create(createMemberDto);
+    // 암호 복호화
+    createMember.password = await bcrypt.hash(createMember.password, 10);
+    const newMember = this.membersRepository.create(createMember);
     await this.membersRepository.save(newMember);
     return { result: true, message: '회원가입이 완료되었습니다.' };
   }
