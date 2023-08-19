faf966f7165d4834a25927832d50edc163608f5a
shimjaedoo <woen1637@naver.com>
2023-08-19 23:16:34 +0900

    [추가] member 회원가입에서 이메일,닉네임,전화번호 유효성검증추가 [추가] 회원관리 조회,추가 서버로직완성

--- src/admin-member/admin-member.controller.ts
+++ src/admin-member/admin-member.controller.ts
@@ -1,14 +1,25 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
 import { AdminMemberService } from './admin-member.service';
 import { IMessage } from '../_common/interfaces/message.interface';
+import { Member } from '../_common/entities';
+import { CreateMemberDto } from '../_common/dtos/members.dto';
 
 @Controller('admin-members')
 export class AdminMemberController {
   constructor(private adminMemberService: AdminMemberService) {}
 
-  // @Get()
-
-  // @Post()
+  // 회원(Member) 조회
+  @Get()
+  @HttpCode(200)
+  async findMember(): Promise<Member[]> {
+    return await this.adminMemberService.findMember();
+  }
+  // 회원(Member) 추가
+  @Post()
+  @HttpCode(201)
+  async createMember(@Body() resisterData: CreateMemberDto): Promise<IMessage> {
+    return await this.adminMemberService.createMember(resisterData);
+  }
 
   // @Patch()
 
