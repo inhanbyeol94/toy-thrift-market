14923d730148a8b23d636b3676517760b08c6927
Kim Jinkyung <jinkyung127@naver.com>
2023-08-19 22:09:14 +0900

    [추가] 관리자 회원 수정 백엔드

--- src/admin-member/admin-member.controller.ts
+++ src/admin-member/admin-member.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
 import { AdminMemberService } from './admin-member.service';
 import { IMessage } from '../_common/interfaces/message.interface';
+import { UpdateAdminMemberDto } from '../_common/dtos/updateAdminMember.dto';
 
 @Controller('admin-members')
 export class AdminMemberController {
@@ -10,7 +11,11 @@ export class AdminMemberController {
 
   // @Post()
 
-  // @Patch()
+  @Patch('/:id')
+  @HttpCode(200)
+  async updateAdminMember(@Param('id') id: number, @Body() data: UpdateAdminMemberDto): Promise<IMessage> {
+    return await this.adminMemberService.updateAdminMember(id, data.name, data.nickname, data.password, data.tel, data.address, data.isAdmin);
+  }
 
   // @Delete()
 }
--- src/admin-member/admin-member.service.ts
+++ src/admin-member/admin-member.service.ts
@@ -2,6 +2,8 @@ import { HttpException, NotFoundException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Member } from '../_common/entities';
+import { IMessage } from '../_common/interfaces/message.interface';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AdminMemberService {
@@ -11,7 +13,25 @@ export class AdminMemberService {
 
   // async createMember(){}
 
-  // async updateMember(){}
+  async updateAdminMember(id: number, name: string, nickname: string, password: string, tel: string, address: string, isAdmin: boolean): Promise<IMessage> {
+    const member = await this.membersRepository.findOne({ where: { id } });
+    if (!member) {
+      throw new NotFoundException('회원이 존재하지 않습니다.');
+    }
+    member.name = name;
+    member.nickname = nickname;
+    member.tel = tel;
+    member.address = address;
+    member.isAdmin = isAdmin;
+
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      member.password = hashedPassword;
+    }
+
+    await this.membersRepository.save(member);
+    return { message: '회원 정보가 수정되었습니다.' };
+  }
 
   // async deleteMember(){}
 }
