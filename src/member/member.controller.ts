a31360158261cc1033580ae676dde43fb26a6519
shimjaedoo <woen1637@naver.com>
2023-08-22 15:24:08 +0900

    [추가] 마이프로필 조회/수정/회원탈퇴/ 비밀번호 변경 서버로직 완성

--- src/member/member.controller.ts
+++ src/member/member.controller.ts
@@ -2,6 +2,9 @@ import { Controller, HttpCode, Get, Post, Body, Patch, Param, Delete } from '@ne
 import { MemberService } from './member.service';
 import { CreateMemberDto } from '../_common/dtos/members.dto';
 import { IMessage } from '../_common/interfaces/message.interface';
+import { UpdateMyPageDto } from '../_common/dtos/updateMypage.dto';
+import { Member } from '../_common/entities';
+import { ChangeDto } from '../_common/dtos/changePw.dto';
 
 @Controller('members')
 export class MemberController {
@@ -11,5 +14,31 @@ export class MemberController {
   @HttpCode(201)
   async createMember(@Body() memberData: CreateMemberDto): Promise<IMessage> {
     return await this.memberService.createMember(memberData);
+  }
+  // 회원정보 조회
+  @Get()
+  @HttpCode(201)
+  async findUser(): Promise<Member> {
+    const id = 4;
+    return this.memberService.findUser(id);
+  }
+  // 회원정보 수정
+  @Patch()
+  @HttpCode(200)
+  async updateMember(@Body() data: UpdateMyPageDto): Promise<IMessage> {
+    const id = 4;
+    return await this.memberService.updateMember(id, data.name, data.nickname, data.tel, data.profileImage, data.address, data.subAddress);
+  }
+  // 비밀번호 변경
+  @Patch('/members/:id/password')
+  @HttpCode(200)
+  async changePassword(@Param('id') memberId: number, @Body() changePW: ChangeDto): Promise<IMessage> {
+    return await this.memberService.changePassword(memberId, changePW.password, changePW.confirmPassword);
+  }
+  // 회원 탈퇴
+  @Delete()
+  @HttpCode(200)
+  async deleteMember(@Param('id') memberId: number): Promise<IMessage> {
+    return await this.memberService.deleteMember(memberId);
   }
 }
--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateMemberDto } from '../_common/dtos/members.dto';
 import { Repository } from 'typeorm';
 import { Member } from '../_common/entities/member.entity';
@@ -25,5 +25,59 @@ export class MemberService {
     const newMember = this.membersRepository.create(createMember);
     await this.membersRepository.save(newMember);
     return { message: '회원가입이 완료되었습니다.' };
+  }
+  // 회원정보 조회
+  async findUser(id: number): Promise<Member> {
+    return await this.membersRepository.findOne({ where: { id } });
+  }
+  // 회원정보 수정
+  async updateMember(id: number, name: string, nickname: string, tel: string, profileImage: string, address: string, subAddress: string): Promise<IMessage> {
+    const member = await this.membersRepository.findOne({ where: { id } });
+    if (!member) {
+      throw new NotFoundException('회원이 존재하지 않습니다.');
+    }
+    const existingNickname = await this.membersRepository.findOne({ where: { nickname } });
+    if (existingNickname) {
+      throw new NotFoundException('이미 존재하는 닉네임입니다.');
+    }
+    const existingTel = await this.membersRepository.findOne({ where: { tel } });
+    if (existingTel) {
+      throw new NotFoundException('이미 존재하는 전화번호입니다.');
+    }
+    member.name = name;
+    member.nickname = nickname;
+    member.tel = tel;
+    member.profileImage = profileImage;
+    member.address = address;
+    member.subAddress = subAddress;
+    await this.membersRepository.save(member);
+    return { message: '회원 정보가 수정되었습니다.' };
+  }
+
+  // 비밀번호 변경
+  async changePassword(memberId: number, password: string, confirmPassword: string): Promise<IMessage> {
+    const member = await this.membersRepository.findOne({ where: { id: memberId } });
+    if (!member) {
+      throw new NotFoundException('회원이 존재하지 않습니다.');
+    }
+
+    const oldPassword = await bcrypt.compare(password, member.password);
+    if (!oldPassword) {
+      throw new NotFoundException('기존 비밀번호가 올바르지 않습니다.');
+    }
+    const newPassword = await bcrypt.hash(confirmPassword, 10);
+    member.password = newPassword;
+    await this.membersRepository.save(member);
+    return { message: '비밀번호가 변경되었습니다.' };
+  }
+  // 회원탈퇴
+  async deleteMember(memberId: number): Promise<IMessage> {
+    const existingMember = await this.membersRepository.findOne({ where: { id: memberId } });
+    if (!existingMember) {
+      throw new NotFoundException('회원이 존재하지 않습니다.');
+    }
+
+    await this.membersRepository.softDelete(memberId);
+    return { message: '회원 탈퇴가 완료되었습니다.' };
   }
 }
