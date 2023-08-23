eb8c51ec724060e646a96068f3a9c8fc7c7709f4
shimjaedoo <woen1637@naver.com>
2023-08-23 23:13:55 +0900

    [수정] bcrypt status 문제로 try-catch문으로 status 500으로 수정

--- src/_common/utils/exception.filters.ts
+++ src/_common/utils/exception.filters.ts
@@ -3,11 +3,20 @@ import { Response } from 'express';
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const status = exception.getStatus(); // 예외코드
-    const message = exception.message;
-    response.status(status).json({ message });
-    console.log(exception.getResponse());
+    try {
+      const ctx = host.switchToHttp();
+      const response = ctx.getResponse<Response>();
+      const status = exception.getStatus(); // 예외코드
+      const message = exception.message;
+      response.status(status).json({ message });
+      console.log(exception);
+    } catch (err) {
+      const ctx = host.switchToHttp();
+      const response = ctx.getResponse<Response>();
+
+      const message = exception.message;
+      response.status(500).json({ message });
+      console.log(exception);
+    }
   }
 }
