d33792e6ddb6a199bf74d81e51f585fbebf4a556
shimjaedoo <woen1637@naver.com>
2023-08-23 23:27:52 +0900

    [추가] uploadService 추가됨

--- src/member/member.module.ts
+++ src/member/member.module.ts
@@ -3,10 +3,11 @@ import { MemberService } from './member.service';
 import { MemberController } from './member.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from '../_common/entities/member.entity';
+import { UploadService } from '../upload/upload.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Member])],
   controllers: [MemberController],
-  providers: [MemberService],
+  providers: [MemberService, UploadService],
 })
 export class MemberModule {}
