313d30c671fe51b81b64a95e965d4e28eab045e1
jegwlee <jegwlee@gmail.com>
2023-08-23 14:52:10 +0900

    [추가] s3 이미지 업로드 미들웨어

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { Pick, Product } from 'src/_common/entities';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UploadMiddleware } from 'src/_common/middlewares/upload.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product, Pick])],
   controllers: [ProductController],
   providers: [ProductService],
 })
-export class ProductModule {}
+export class ProductModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(UploadMiddleware).forRoutes({ path: '/products', method: RequestMethod.POST });
+    // consumer.apply(TokenValidMiddleware).forRoutes(BoardsController);
+  }
+}
