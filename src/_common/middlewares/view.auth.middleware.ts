8efa4790e97f3f325c23fe581f7f105a41469d1e
inhanbyeol <inhanbyeol94@gmail.com>
2023-08-23 21:06:50 +0900

    [추가] 토큰 검증 미들웨어

--- src/_common/middlewares/view.auth.middleware.ts
+++ src/_common/middlewares/view.auth.middleware.ts
@@ -1,12 +1,11 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { IRequest } from '../interfaces/request.interface';
-import { NextFunction, Response, Request } from 'express';
+import { NextFunction, Response } from 'express';
 
 @Injectable()
-export class TestMiddleware implements NestMiddleware {
-  constructor() {}
-
+export class ViewAuthMiddleware implements NestMiddleware {
   async use(req: IRequest, res: Response, next: NextFunction) {
+    if (!req.user) return res.redirect('/login');
     return next();
   }
 }
