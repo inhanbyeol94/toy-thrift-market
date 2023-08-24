691eb42f1473186c553f5f1cc285f33434f17f5c
shimjaedoo <woen1637@naver.com>
2023-08-24 17:32:30 +0900

    [추가] JWTService , tokenValidMiddleware

--- src/member/member.module.ts
+++ src/member/member.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { MemberService } from './member.service';
 import { MemberController } from './member.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from '../_common/entities/member.entity';
 import { UploadService } from '../upload/upload.service';
+import { TokenValidMiddleware } from '../_common/middlewares/token.valid.middleware';
+
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Member])],
   controllers: [MemberController],
-  providers: [MemberService, UploadService],
+  providers: [MemberService, UploadService, JwtService],
 })
-export class MemberModule {}
+export class MemberModule {
+  configure(consumer: MiddlewareConsumer) {
+    /* 토큰 미들웨어 */
+    consumer.apply(TokenValidMiddleware).forRoutes(MemberController);
+  }
+}
