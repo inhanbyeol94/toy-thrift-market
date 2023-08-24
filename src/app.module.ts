1600202d309ca28acc030e369343af428ce03b43
inhanbyeol <inhanbyeol94@gmail.com>
2023-08-24 20:59:57 +0900

    [추가] cacheManager RedisStore 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,11 +14,25 @@ import { CommentModule } from './comment/comment.module';
 import { SlackModule } from './slack/slack.module';
 import { UploadModule } from './upload/upload.module';
 import { ProductModule } from './product/product.module';
+import { IdentityModule } from './identity/identity.module';
+import * as redisStore from 'cache-manager-redis-store';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({ useFactory: ormConfig }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async () => {
+        return {
+          store: redisStore,
+          host: process.env.REDIS_HOST,
+          port: process.env.REDIS_PORT,
+          // password: process.env.REDIS_PASSWORD,
+        };
+      },
+    }),
     ViewModule,
     MemberModule,
     AuthModule,
@@ -31,6 +45,7 @@ import { ProductModule } from './product/product.module';
     SlackModule,
     UploadModule,
     ProductModule,
+    IdentityModule,
   ],
   controllers: [],
   providers: [],
