3b4540d0b18504d00cf1c5d5bacd2dff7e25fa2f
inhanbyeol <inhanbyeol94@gmail.com>
2023-08-24 23:15:59 +0900

    [변경] senquence 변경

--- src/identity/identity.controller.ts
+++ src/identity/identity.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { ISequence } from 'src/_common/interfaces/sequence.interface';
 import { IMessage } from '../_common/interfaces/message.interface';
 import { IdentityService } from './identity.service';
 import { VerificationRequestDto } from '../_common/dtos/identityVerificationRequest.dto';
@@ -9,12 +10,14 @@ export class IdentityController {
   constructor(private identityService: IdentityService) {}
 
   @Post()
-  async sendAuthentication(@Body() identityData: VerificationRequestDto): Promise<IMessage> {
+  @HttpCode(200)
+  async sendAuthentication(@Body() identityData: VerificationRequestDto): Promise<ISequence> {
     return await this.identityService.sendAuthentication(identityData);
   }
 
   @Post('verify')
-  async verifyIdentity(@Body() verifyData: IdentityVerifyDto): Promise<IMessage> {
+  @HttpCode(200)
+  async verifyIdentity(@Body() verifyData: IdentityVerifyDto): Promise<ISequence> {
     return await this.identityService.verifyAuthentication(verifyData);
   }
 }
