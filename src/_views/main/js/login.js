9474c03e2a97c721cf72bb6b08d1467f1d56bbff
shimjaedoo <woen1637@naver.com>
2023-08-24 20:36:59 +0900

    [추가] 로그인 검증

--- src/_views/main/js/login.js
+++ src/_views/main/js/login.js
@@ -23,6 +23,11 @@ loginBtnEl.addEventListener('click', async () => {
       window.location.href = '/';
     } else {
       console.error(data.message);
+      if (data.message === '비밀번호가 틀렸습니다.') {
+        alert('비밀번호가 틀렸습니다. 다시 시도해주세요.');
+      } else {
+        alert('이메일 주소와 비밀번호를 확인해주세요.');
+      }
     }
   } catch (error) {
     alert('로그인 도중 오류가 발생했습니다.');
