c2e4bb7de2a8411046e6f64a2d4522429af7a98a
shimjaedoo <woen1637@naver.com>
2023-08-26 22:39:46 +0900

    [수정] Schema edit

--- src/_common/schema/like.schema.ts
+++ src/_common/schema/like.schema.ts
@@ -27,9 +27,31 @@ export class MainPage {
       icon: String,
       title: String,
       content: String,
+      icon2: String,
+      title2: String,
+      content2: String,
+      icon3: String,
+      title3: String,
+      content3: String,
+      icon4: String,
+      title4: String,
+      content4: String,
     },
   ])
-  columns: { icon: string; title: string; content: string }[];
+  columns: {
+    icon: string;
+    title: string;
+    content: string;
+    icon2: string;
+    title2: string;
+    content2: string;
+    icon3: string;
+    title3: string;
+    content3: string;
+    icon4: string;
+    title4: string;
+    content4: string;
+  }[];
 
   isAdmin: boolean;
 }
