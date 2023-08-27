44b50bf2b212905a59f763525b21f90da6b7dbe2
shimjaedoo <woen1637@naver.com>
2023-08-27 17:03:46 +0900

    [추가] 메인보드 DTO

--- src/_views/admin/assets/js/Admin-mainboard.js
+++ src/_views/admin/assets/js/Admin-mainboard.js
@@ -16,62 +16,136 @@ const clcontentThree = document.getElementById('cl3content');
 const bottomcliconFour = document.getElementById('cl4Icon');
 const cltitleFour = document.getElementById('cl4Title');
 const clcontentFour = document.getElementById('cl4content');
+const imgView = document.getElementById('imgView');
+
+const access_token = localStorage.getItem('access_token');
+
 // 메인대시보드 정보 가져오기
 async function maindashBoard() {
   try {
-    const res = await fetch('/admin-mainboards', { method: 'GET' });
-
+    const res = await fetch('/admin-mainboards', { method: 'GET', headers: { Authorization: `Bearer ${access_token}` } });
     if (!res.ok) {
       console.error('Response status:', res.status);
       throw new Error('정보를 가져오는데 실패하였습니다.');
     }
 
-    const [dashBoard] = await res.json();
+    const dashBoard = await res.json();
     console.log(dashBoard);
-    imageTag.setAttribute('src', dashBoard.image);
-    maintitleTag.value = dashBoard.mainTitle;
-    subtitleTag.value = dashBoard.subTitle;
-    bottomtaptitleTag.value = dashBoard.bottomTitle;
-    bottomtapcolorTag.value = dashBoard.bottomtapColor;
+    imgView.setAttribute('src', dashBoard[0].mainImage);
+    maintitleTag.value = dashBoard[0].mainTitle;
+    subtitleTag.value = dashBoard[0].subTitle;
+    bottomtapcolorTag.value = dashBoard[0].bottomtapColor;
+    bottomtaptitleTag.value = dashBoard[0].bottomTitle;
 
-    bottomcliconOne.value = dashBoard.columns[0].icon;
-    cltitleOne.value = dashBoard.columns[0].title;
-    clcontentOne.value = dashBoard.columns[0].content;
+    bottomcliconOne.value = dashBoard[0].icon1;
+    cltitleOne.value = dashBoard[0].title1;
+    clcontentOne.value = dashBoard[0].content1;
 
-    bottomcliconTwo.value = dashBoard.columns[1].icon;
-    cltitleTwo.value = dashBoard.columns[1].title;
-    clcontentTwo.value = dashBoard.columns[1].title;
+    bottomcliconTwo.value = dashBoard[0].icon2;
+    cltitleTwo.value = dashBoard[0].title2;
+    clcontentTwo.value = dashBoard[0].content2;
 
-    bottomcliconThree.value = dashBoard.columns[2].icon;
-    cltitleThree.value = dashBoard.columns[2].title;
-    clcontentThree.value = dashBoard.columns[2].content;
+    bottomcliconThree.value = dashBoard[0].icon3;
+    cltitleThree.value = dashBoard[0].title3;
+    clcontentThree.value = dashBoard[0].content3;
 
-    bottomcliconFour.value = dashBoard.columns[3].icon;
-    cltitleFour.value = dashBoard.columns[3].title;
-    clcontentFour.value = dashBoard.columns[3].content;
-
-    let formData = new FormData();
-    formData.append('imageTag', dashBoard.image);
-    formData.append('maintitleTag', maintitleTag.value);
-    formData.append('subtitleTag', subtitleTag.value);
-    formData.append('bottomtaptitleTag', bottomtaptitleTag.value);
-    formData.append('bottomtapcolorTag', bottomtapcolorTag.value);
-    formData.append('bottomcliconOne', bottomcliconOne.value);
-    formData.append('cltitleOne', cltitleOne.value);
-    formData.append('clcontentOne', clcontentOne.value);
-    formData.append('bottomcliconTwo', bottomcliconTwo.value);
-    formData.append('cltitleTwo', cltitleTwo.value);
-    formData.append('clcontentTwo', clcontentTwo.value);
-    formData.append('bottomcliconThree', bottomcliconThree.value);
-    formData.append('bottomcliconTwo', bottomcliconTwo.value);
-    formData.append('cltitleThree', cltitleThree.value);
-    formData.append('clcontentThree', clcontentThree.value);
-    formData.append('bottomcliconFour', bottomcliconFour.value);
-    formData.append('cltitleFour', cltitleFour.value);
-    formData.append('clcontentFour', clcontentFour.value);
+    bottomcliconFour.value = dashBoard[0].icon4;
+    cltitleFour.value = dashBoard[0].title4;
+    clcontentFour.value = dashBoard[0].content4;
   } catch (error) {
     console.error(error);
   }
 }
 maindashBoard();
-// document.getElementById('dashboardEdit').addEventListener('click');
+
+async function updateMaindashBoard() {
+  const data = new FormData();
+
+  data.append('images', imageTag.files[0]); // 메인 이미지
+
+  data.append('mainTitle', maintitleTag.value); // 메인 제목
+  data.append('subTitle', subtitleTag.value); // 서브 제목
+
+  data.append('bottomTitle', bottomtaptitleTag.value); //하단 제목
+  data.append('bottomColor', bottomtapcolorTag.value); //하단 색상
+
+  data.append('icon1', bottomcliconOne.value);
+  data.append('icon2', bottomcliconTwo.value);
+  data.append('icon3', bottomcliconThree.value);
+  data.append('icon4', bottomcliconFour.value);
+
+  data.append('title1', cltitleOne.value);
+  data.append('title2', cltitleTwo.value);
+  data.append('title3', cltitleThree.value);
+  data.append('title4', cltitleFour.value);
+
+  data.append('content1', clcontentOne.value);
+  data.append('content2', clcontentTwo.value);
+  data.append('content3', clcontentThree.value);
+  data.append('content4', clcontentFour.value);
+
+  const api = await fetch('/admin-mainboards', {
+    method: 'PATCH',
+    body: data,
+  });
+
+  const { status } = await api;
+  const result = await api.json();
+  alert(result.message);
+  if (status === 200) window.location.reload();
+}
+async function eqwe() {
+  // S3
+  let formData = new FormData();
+
+  formData.append('images', imageTag.files[0]);
+  console.log(imageTag.files[0]);
+  formData.append('mainTitle', maintitleTag.value);
+  formData.append('subTitle', subtitleTag.value);
+  formData.append('bottomTabColor', bottomtapcolorTag.value);
+  formData.append('bottomTitle', bottomtaptitleTag.value);
+  const columns = [
+    {
+      icon: bottomcliconOne.value,
+      title: cltitleOne.value,
+      content: clcontentOne.value,
+    },
+    {
+      icon: bottomcliconTwo.value,
+      title: cltitleTwo.value,
+      content: clcontentTwo.value,
+    },
+    {
+      icon: bottomcliconThree.value,
+      title: cltitleThree.value,
+      content: clcontentThree.value,
+    },
+    {
+      icon: bottomcliconFour.value,
+      title: cltitleFour.value,
+      content: clcontentFour.value,
+    },
+  ];
+  formData.append('columns', JSON.stringify(columns));
+
+  try {
+    const id = '64e9bae4516f0e147346eb13';
+    const response = await fetch(`/admin-mainboards/${id}`, {
+      method: 'PATCH',
+      headers: { Authorization: `Bearer ${access_token}` },
+      body: formData,
+    });
+    const result = await response.json();
+
+    if (response.ok) {
+      alert(result.message);
+      window.location.reload();
+    } else {
+      alert(result.message || '메인대시보드 수정에 실패하였습니다.');
+    }
+  } catch (error) {
+    console.error(error);
+    alert('요청 처리중 오류가 발생하였습니다.');
+  }
+}
+document.getElementById('dashboardEdit').addEventListener('click', updateMaindashBoard);
---
+++ src/_views/main/js/main-dashboard-manage.js
@@ -0,0 +1,44 @@
+const loadmainboardInfo = async () => {
+  fetch('admin-mainboards', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const mainTitle = document.getElementById('mainTitle');
+      const subTitle = document.getElementById('subTitle');
+      const bottomTitle = document.getElementById('bottomTitle');
+      const col1Icon = document.getElementById('col1Icon');
+      const col1Title = document.getElementById('col1Title');
+      const col1content = document.getElementById('col1content');
+      const col2Icon = document.getElementById('col2Icon');
+      const col2Title = document.getElementById('col2Title');
+      const col2content = document.getElementById('col2content');
+      const col3Icon = document.getElementById('col3Icon');
+      const col3Title = document.getElementById('col3Title');
+      const col3content = document.getElementById('col3content');
+      const col4Icon = document.getElementById('col4Icon');
+      const col4Title = document.getElementById('col4Title');
+      const col4content = document.getElementById('col4content');
+
+      mainTitle.innerText = data[0].mainTitle;
+      subTitle.innerText = data[0].subTitle;
+      bottomTitle.innerText = data[0].bottomTitle;
+      col1Title.innerText = data[0].columns[0].title;
+      col2Title.innerText = data[0].columns[1].title;
+      col3Title.innerText = data[0].columns[2].title;
+      col4Title.innerText = data[0].columns[3].title;
+      col1content.innerText = data[0].columns[0].content;
+      col2content.innerText = data[0].columns[1].content;
+      col3content.innerText = data[0].columns[2].content;
+      col4content.innerText = data[0].columns[3].content;
+      col1Icon.innerHTML = `<i class=${data[0].columns[0].icon}></i>`;
+      col2Icon.innerHTML = `<i class=${data[0].columns[1].icon}></i>`;
+      col3Icon.innerHTML = `<i class=${data[0].columns[2].icon}></i>`;
+      col4Icon.innerHTML = `<i class=${data[0].columns[3].icon}></i>`;
+    });
+};
+loadmainboardInfo();
