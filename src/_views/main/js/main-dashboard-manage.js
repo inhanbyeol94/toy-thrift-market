85f1cc300ef3023061c7fad7db7be48d6b027493
Kim Jinkyung <jinkyung127@naver.com>
2023-08-27 17:57:15 +0900

    [변경] 데이터에 맞춰 경로 변경, 하단 아이콘 로직 수정

--- src/_views/main/js/main-dashboard-manage.js
+++ src/_views/main/js/main-dashboard-manage.js
@@ -7,12 +7,10 @@ const loadmainboardInfo = async () => {
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-
-      // const mainSection = document.querySelector('.bg-accent');
+      const mainSection = document.querySelector('.bg-accent');
       const mainTitle = document.getElementById('mainTitle');
       const subTitle = document.getElementById('subTitle');
-      // const bottomTapColor = document.getElementById('bottomTapColor');
+      const bottomTapColor = document.getElementById('bottomTapColor');
       const bottomTitle = document.getElementById('bottomTitle');
       const col1Icon = document.getElementById('col1Icon');
       const col1Title = document.getElementById('col1Title');
@@ -27,23 +25,23 @@ const loadmainboardInfo = async () => {
       const col4Title = document.getElementById('col4Title');
       const col4content = document.getElementById('col4content');
 
-      // mainSection.style.backgroundImage = `url(${data[0].mainImage})`;
+      mainSection.style.backgroundImage = `url(${data[0].mainImage})`;
       mainTitle.innerText = data[0].mainTitle;
       subTitle.innerText = data[0].subTitle;
       bottomTitle.innerText = data[0].bottomTitle;
-      // bottomTapColor.style.backgroundColor = data[0].bottomColor;
-      col1Title.innerText = data[0].columns[0].title;
-      col2Title.innerText = data[0].columns[1].title;
-      col3Title.innerText = data[0].columns[2].title;
-      col4Title.innerText = data[0].columns[3].title;
-      col1content.innerText = data[0].columns[0].content;
-      col2content.innerText = data[0].columns[1].content;
-      col3content.innerText = data[0].columns[2].content;
-      col4content.innerText = data[0].columns[3].content;
-      col1Icon.setAttribute('class', `${data[0].columns[0].icon}`);
-      col2Icon.setAttribute('class', `${data[0].columns[1].icon}`);
-      col3Icon.setAttribute('class', `${data[0].columns[2].icon}`);
-      col4Icon.setAttribute('class', `${data[0].columns[3].icon}`);
+      bottomTapColor.style.backgroundColor = data[0].bottomColor;
+      col1Title.innerText = data[0].title1;
+      col2Title.innerText = data[0].title2;
+      col3Title.innerText = data[0].title3;
+      col4Title.innerText = data[0].title4;
+      col1content.innerText = data[0].content1;
+      col2content.innerText = data[0].content2;
+      col3content.innerText = data[0].content3;
+      col4content.innerText = data[0].content4;
+      col1Icon.setAttribute('class', `h2 text-body bottomIcon ${data[0].icon1}`);
+      col2Icon.setAttribute('class', `h2 text-body bottomIcon ${data[0].icon2}`);
+      col3Icon.setAttribute('class', `h2 text-body bottomIcon ${data[0].icon3}`);
+      col4Icon.setAttribute('class', `h2 text-body bottomIcon ${data[0].icon4}`);
     });
 };
 loadmainboardInfo();
