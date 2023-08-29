f940438a69fdd0924ed24db46023811b28e63d7e
Kim Jinkyung <jinkyung127@naver.com>
2023-08-29 10:32:11 +0900

    [추가] 찜목록, 찜버튼 연동

--- src/_views/main/js/pick.add.delete.js
+++ src/_views/main/js/pick.add.delete.js
@@ -1,17 +1,10 @@
 const currentURL = window.location.href;
 const currentProductId = currentURL.split('product/').pop();
-console.log(currentURL, currentProductId);
+
 // '찜하기' 버튼 클릭 이벤트 핸들러
 const pickbutton = document.getElementById('pickbutton');
+const lickbutton = document.getElementById('likeColor');
 
-function setPickButtonColor(piced) {
-  const lickbutton = document.getElementById('likeColor');
-  if (piced) {
-    lickbutton.style.color = '#FFBF00';
-  } else {
-    lickbutton.style.color = '';
-  }
-}
 async function onPickButtonClick(productId) {
   try {
     const response = await fetch(`/pick/${productId}`, {
@@ -24,32 +17,31 @@ async function onPickButtonClick(productId) {
       throw new Error('찜하기 요청 실패');
     }
     const result = await response.json();
-
-    const piced = result.message === '찜목록에 추가되었습니다.' ? true : false;
-    setPickButtonColor(piced);
     alert(result.message);
+    window.location.reload();
   } catch (error) {
     console.error(error);
   }
 }
+
 pickbutton.addEventListener('click', () => onPickButtonClick(currentProductId));
-async function myPick(productId) {
+
+async function myPick() {
   try {
-    const res = await fetch('/products/picks', {
+    const response = await fetch('/products/picks', {
       method: 'GET',
       headers: { 'Content-Type': 'application/json' },
     });
-    // console.log(res);
-    if (!res.ok) {
-      throw new Error('찜목록 조회실패');
+    const data = await response.json();
+
+    const matchingItems = data.filter((item) => item.id === currentProductId);
+    if (matchingItems.length > 0) {
+      lickbutton.style.color = '#FFBF00';
+    } else {
+      lickbutton.style.color = '';
     }
-    const picks = await res.json();
-    const pickProducts = picks.map((pick) => pick.productId);
-    // console.log(pickProducts);
-    const isPicked = pickProducts.includes(productId);
-    setPickButtonColor(isPicked);
   } catch (err) {
     console.error(err);
   }
 }
-window.onload = () => myPick(currentProductId);
+myPick();
