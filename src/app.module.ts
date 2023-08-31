f9e19671d39d541e122046505850af41d076bae9
Kim Jinkyung <jinkyung127@naver.com>
2023-08-31 19:22:34 +0900

    [추가] 리뷰 작성, 수정 api

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,6 +25,7 @@ import { PickModule } from './pick/pick.module';
 import { PaymembercheckModule } from './paymembercheck/paymembercheck.module';
 import * as process from 'process';
 import { HanbyeolBankModule } from './hanbyeol-bank/hanbyeol-bank.module';
+import { ReViewModule } from './re-view/review.module';
 
 @Module({
   imports: [
@@ -61,6 +62,7 @@ import { HanbyeolBankModule } from './hanbyeol-bank/hanbyeol-bank.module';
     PickModule,
     PaymembercheckModule,
     HanbyeolBankModule,
+    ReViewModule,
   ],
   controllers: [],
   providers: [],
