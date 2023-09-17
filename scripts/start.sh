a0b25b0e95e93a9976ab4bb9ea9a015541b26fab
jegwlee <jegwlee@gmail.com>
2023-09-17 18:55:58 +0900

    [추가] ec2에 env파일 생성

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,6 +7,9 @@ TIME_NOW=$(date +%c)
 
 cd $PROJECT_ROOT
 
+ENV_CONTENT=$(aws ssm get-parameter --name ENV_FILE --with-decryption --query "Parameter.Value" --output text)
+echo "$ENV_CONTENT" > .env
+
 pm2 delete $APP_NAME
 pm2 start npm --name $APP_NAME -- start
 
